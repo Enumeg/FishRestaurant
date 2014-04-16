6d9b2509a41eebbc70cd55e6bf41773350cea204
dev-mustafa <m.hussein@moveitgate.com>
2014-04-16 22:09:44 +0200

    Purchases page

--- FishRestaurant.WPF/Purchase.xaml.cs
+++ FishRestaurant.WPF/Purchase.xaml.cs
@@ -1,46 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using Source;
 using FishRestaurant.Model.Entities;
+using System.Data.Entity;
+using Source;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
 
 namespace FishRestaurant.WPF
 {
     /// <summary>
     /// Interaction logic for Product.xaml
     /// </summary>
-    public partial class Purchase : Page
+    public partial class Purchases : Page
     {
-        FRContext DBContext;
-        public Purchase()
+        FRContext DB;
+        public Purchases()
         {
             InitializeComponent();
-            DBContext = new FRContext();
-            Fill_Patients_LB();
+            DB = new FRContext();
+            FillLB();
             InitializeLookups();
+            UnitCB.ItemsSource = Enum.GetValues(typeof(Units));
         }
 
         private void InitializeLookups()
         {
             try
             {
-                //Company_CB.ItemsSource = Health_Company_CB.ItemsSource = DBContext.companies.OrderBy(c => c.name).ToList();
-                //Gender_CB.ItemsSource = Enum.GetValues(typeof(Gender));
+                ComponentCB.ItemsSource = DB.Components.OrderBy(c => c.Name).ToList();
+                SupplierCB.ItemsSource = DB.Suppliers.OrderBy(c => c.Name).ToList();
+                var suppliers = DB.Suppliers.OrderBy(c => c.Name).ToList();
+                suppliers.Insert(0, new Supplier() { Id = 0, Name = "الكل" });
+                SupplierSearch.ItemsSource = suppliers;
             }
             catch
             {
 
             }
         }
-        private void Fill_Patients_LB()
+        private void FillLB()
         {
 
             try
             {
-                //Patients_LB.ItemsSource = DBContext.patients.Where(p => p.name.StartsWith(Name_TB.Text) && p.phone.StartsWith(Tele_TB.Text)).OrderBy(P => P.name).ToList();
+
+                var query = DB.Purchases.AsQueryable();
+                if (SupplierSearch.SelectedIndex > 0) { query = query.Where(p => p.SupplierId == (int)SupplierSearch.SelectedValue); }
+                if (DateSearch.Text != "")
+                {
+                    var date = DateSearch.Value.Value.Date;
+                    query = query.Where(p => p.Date == date);
+                }
+                LB.ItemsSource = query.Include(p => p.PurchaseDetails).OrderBy(p => p.Number).ToList();
+
             }
             catch
             {
@@ -48,69 +62,83 @@ namespace FishRestaurant.WPF
             }
 
         }
-
         private void EditPanel_Edit(object sender, EventArgs e)
         {
             try
             {
-                //if (((Button)sender).Name.Split('_')[0] == "Add")
-                //{
-                //    Patients_LB.SelectedIndex = -1;
-                //    Main_Grid.DataContext = new Patient() { gender = Gender.ذكر };
-                //    Form.Set_Style(Main_Grid, Operations.Add);
-                //}
-                //else
-                //{
-                //    Form.Set_Style(Main_Grid, Operations.Edit);
-                //}
-                //Main_Grid.RowDefinitions[4].Height = new GridLength(35);
-                //Patients_LB.IsEnabled = false;
+                MainGrid.ColumnDefinitions[0].Width = new GridLength(0);
+                LB.IsEnabled = false;
+                ViewGrid.RowDefinitions[3].Height = new GridLength(35);
+                EditGrid.DataContext = new PurchaseDetail() { Amount = 1 };
+                Details_DG.ColumnHeaderHeight = 62;
+                Details_GD.RowDefinitions[1].Height = new GridLength(28);
+                Details_DG.IsReadOnly = false;
+                if (((Button)sender).Name.Split('_')[0] == "Add")
+                {
+                   
+                    LB.SelectedIndex = -1;                    
+                    ViewGrid.DataContext = new Purchase() { Date = DateTime.Now.Date, Type = Transaction_Types.In };
+                    Form.Set_Style(InfoGrid, Operations.Add);
+                    Form.Set_Style(TotalsGrid, Operations.Add);
+                    GetNumber();
+                }
+                else
+                {
+                    Form.Set_Style(InfoGrid, Operations.Edit);
+                    Form.Set_Style(TotalsGrid, Operations.Edit);
+                }
+
             }
             catch
             {
 
             }
         }
-
         private void EditPanel_Delete(object sender, EventArgs e)
         {
             try
             {
-                //if (Patients_LB.SelectedIndex != -1)
-                //{
+                if (LB.SelectedIndex != -1)
+                {
 
-                //    if (Message.Show("هل تريد حذف هذا العميل", MessageBoxButton.YesNoCancel, 5) == MessageBoxResult.Yes)
-                //    {
-                //        DBContext.patients.Remove((Patient)Patients_LB.SelectedItem);
-                //        DBContext.SaveChanges();
-                //        Fill_Patients_LB();
-                //    }
-                //}
+                    if (Message.Show("هل تريد حذف هذه الفاتورة", MessageBoxButton.YesNoCancel, 5) == MessageBoxResult.Yes)
+                    {
+                        DB.Purchases.Remove((Purchase)LB.SelectedItem);
+                        DB.SaveChanges();
+                        FillLB();
+                    }
+                }
             }
             catch
             {
 
             }
         }
-
         private void Submit(object sender, EventArgs e)
         {
             try
             {
-                //if (((Button)sender).Name.Split('_')[0] == "Save")
-                //{
-                //    if (Patients_LB.SelectedIndex == -1)
-                //    {
-                //        DBContext.patients.Add((Patient)Main_Grid.DataContext);
-                //    }
-                //    DBContext.SaveChanges();
-                //}
-                //Main_Grid.SetBinding(DataContextProperty, new Binding() { ElementName = "Patients_LB", Path = new PropertyPath("SelectedItem") });
-                //Main_Grid.RowDefinitions[4].Height = new GridLength(0);
-                //Patients_LB.IsEnabled = true;
-                //Fill_Patients_LB();
-                //Form.Set_Style(Main_Grid, Operations.View);
-                //Confirm.Check(true);
+
+                if (((Button)sender).Name.Split('_')[0] == "Save")
+                {
+                    if (Notify.validate("من فضلك أدخل المورد", SupplierCB.SelectedIndex, Main.GetWindow(this))) { return; }
+                    if (LB.SelectedIndex == -1)
+                    {
+                        DB.Purchases.Add((Purchase)ViewGrid.DataContext);
+                    }
+                    DB.SaveChanges();
+                    Confirm.Check(true);
+                }
+                Details_DG.IsReadOnly = true;
+                LB.IsEnabled = true;
+                FillLB();
+                MainGrid.ColumnDefinitions[0].Width = new GridLength(1, GridUnitType.Star);
+                ViewGrid.RowDefinitions[3].Height = new GridLength(0);
+                Form.Set_Style(InfoGrid, Operations.View);
+                Form.Set_Style(TotalsGrid, Operations.View);
+                Details_DG.ColumnHeaderHeight = 32;
+                Details_GD.RowDefinitions[1].Height = new GridLength(0);
+
             }
             catch
             {
@@ -122,6 +150,85 @@ namespace FishRestaurant.WPF
         {
             try
             {
+                ViewGrid.DataContext = LB.SelectedItem;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void FillLB(object sender, EventArgs e)
+        {
+            FillLB();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (AddBTN.Content.ToString() == "Add")
+                {
+                    var PurchaseDetail = (PurchaseDetail)EditGrid.DataContext;
+                    var oldPurchaseDetail = ((Purchase)ViewGrid.DataContext).PurchaseDetails.FirstOrDefault(p => p.Component== PurchaseDetail.Component && p.Unit == PurchaseDetail.Unit);
+                    if (oldPurchaseDetail != null) { oldPurchaseDetail.Amount += PurchaseDetail.Amount; }
+                    else
+                        ((Purchase)ViewGrid.DataContext).PurchaseDetails.Add(PurchaseDetail);
+                }
+                AddBTN.Content = "Add";
+
+                Total_TB.Text = Paid_TB.Text = ((Purchase)ViewGrid.DataContext).PurchaseDetails.Sum(p => (p.Price * p.Amount)).ToString("0.00");
+                Details_DG.ItemsSource = null;
+                Details_DG.SetBinding(DataGrid.ItemsSourceProperty, "PurchaseDetails");
+                EditGrid.DataContext = new PurchaseDetail() { Amount = 1 };
+            }
+            catch
+            {
+
+            }
+        }
+        private void Details_DG_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (Details_DG.SelectedIndex != -1)
+                {
+                    EditGrid.DataContext = Details_DG.SelectedItem;
+                    AddBTN.Content = "Edit";
+                }
+                else
+                {
+                    EditGrid.DataContext = new PurchaseDetail() { Amount = 1 };
+                    AddBTN.Content = "Add";
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void GetNumber()
+        {
+            try
+            {
+                if (!LB.IsEnabled)
+                {
+                    var date = DateDTP.Value.Value.Date;
+                    var num = DB.Purchases.Where(p => p.Date.Year == date.Year && p.Date.Month == date.Month);
+                    Number.Text = num.Count() != 0 ? (num.Max(p => p.Number) + 1).ToString() : string.Format("{0}{1}001", date.Year, date.Month);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Component_CB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
 
             }
             catch
@@ -130,10 +237,36 @@ namespace FishRestaurant.WPF
             }
         }
 
-        private void Fill_LB(object sender, TextChangedEventArgs e)
+        private void Details_DG_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
-            Fill_Patients_LB();
+            try
+            {
+                if (!Details_DG.IsReadOnly)
+                {
+                    ((Purchase)ViewGrid.DataContext).PurchaseDetails.Remove((PurchaseDetail)EditGrid.DataContext);
+                    Details_DG.ItemsSource = null;
+                    Details_DG.SetBinding(DataGrid.ItemsSourceProperty, "PurchaseDetails");
+                }
+            }
+            catch
+            {
+
+            }
         }
+
+        private void Paid_TB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+              var Purchase =((Purchase)ViewGrid.DataContext);
+              Rest_TB.Text = (Purchase.Total - Purchase.Paid).ToString("0.00");
+            }
+            catch
+            {
+
+            }
+        }
+
 
 
     }

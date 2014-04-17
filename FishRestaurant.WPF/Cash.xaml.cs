54d6427960469d6f67e2c7c389a01472da24705e
it-m-atef <it.m.atef@gmail.com>
2014-04-16 22:36:52 -0700

    Pages code

---
+++ FishRestaurant.WPF/Cash.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FishRestaurant.WPF
+{
+    /// <summary>
+    /// Interaction logic for Cash.xaml
+    /// </summary>
+    public partial class Cash : Window
+    {
+        public Cash()
+        {
+            InitializeComponent();
+        }
+
+        private void Sell_Back_BTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Sell_BTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

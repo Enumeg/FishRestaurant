e440f4dcc23ce019450779df67602c987b7c0ad3
it-m-atef <it.m.atef@gmail.com>
2014-04-17 08:32:00 -0700

    customer_Supplier_Pages

--- FishRestaurant.WPF/Customers.xaml.cs
+++ FishRestaurant.WPF/Customers.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Source;
+using FishRestaurant.Model.Entities;
 
 namespace FishRestaurant.WPF
 {
@@ -20,12 +22,99 @@ namespace FishRestaurant.WPF
     /// </summary>
     public partial class Customers : Page
     {
+        FRContext DB;
+
         public Customers()
         {
             InitializeComponent();
+            DB = new FRContext();
+            Fill_LB();
         }
-    
-       
-    
+
+        private void Fill_LB()
+        {
+            try
+            {
+                if (LB.IsEnabled)
+                {
+                    LB.ItemsSource = DB.Customers.Where(c => c.Name.StartsWith(NameSearchTB.Text) && c.Mobile.StartsWith(MobileSearchTB.Text)).OrderBy(o => o.Name).ToList();
+                }
+            }
+            catch
+            {
+                
+                
+            }
+        // End Fill
+        }
+
+
+
+
+        private void EditPanel_Edit(object sender, EventArgs e)
+        {
+            try
+            {
+                if (((Button)sender).Name.Split('_')[0] == "Add")
+                {
+                    LB.SelectedIndex = -1;
+                }
+             
+                LB.IsEnabled = false;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void EditPanel_Delete(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (Message.Show("هل تريد حذف هذا العميل", MessageBoxButton.YesNoCancel, 10) == MessageBoxResult.Yes)
+                {
+                    DB.Customers.Remove((Customer)LB.SelectedItem);
+                    DB.SaveChanges();
+                    Fill_LB();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+
+
+        private void Submit(object sender, EventArgs e)
+        {
+            try
+            {
+                if (((Button)sender).Name.Split('_')[0] == "Save")
+                {
+                    if (LB.SelectedIndex == -1) 
+                    { 
+                        DB.Customers.Add(new Customer() { Name = NameTB.Text, Address = AddressTB.Text,Phone = TelephoneTB.Text,Mobile = MobileTB.Text }); 
+                    }
+
+                    DB.SaveChanges();
+                    Confirm.Check(true);
+                }
+
+
+                    NameTB.Text = AddressTB.Text = TelephoneTB.Text = MobileTB.Text = "";  
+                    LB.IsEnabled = true;
+                    Fill_LB();
+                
+
+            }
+            catch
+            {
+                Confirm.Check(false);
+            }
+        }
+
     }
 }

276dc67a2b333c211fb5a25c2f829ca6db94d175
it-m-atef <it.m.atef@gmail.com>
2014-04-29 15:00:26 -0700

    Changes

---
+++ FishRestaurant.WPF/Reception.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FishRestaurant.WPF
+{
+    /// <summary>
+    /// Interaction logic for Reception.xaml
+    /// </summary>
+    public partial class Reception : Window
+    {
+        public Reception()
+        {
+            InitializeComponent();
+        }
+
+        private void Customer_BTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+      
+        private void Sales_BTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

5a29d3c305e2efd36b0f08be3be778e0a74e294c
it-m-atef <it.m.atef@gmail.com>
2014-04-29 11:55:30 -0700

    changes

---
+++ FishRestaurant.Model/Entities/Outcome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishRestaurant.Model.Entities
+{
+    class Outcome
+    {
+
+        public Outcome()
+        { 
+         
+
+
+        }
+
+
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Value { get; set; }
+
+
+        public string Description { get; set; }
+
+
+        public int OutcomeTypesId { get; set; }
+
+        public virtual OutcomeType OutcomeType { get; set; }
+
+    }
+
+       
+
+}
---
+++ FishRestaurant.Model/Entities/Outcome_Types.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishRestaurant.Model.Entities
+{
+    class OutcomeType
+    {
+
+
+        public OutcomeType()
+        {
+           
+            Outcomes = new List<Outcome>();
+        
+        }
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Outcome> Outcomes { get; set; }
+
+    }
+
+
+    
+}
